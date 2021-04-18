be835a37976f73491b6feec45134ad72a59e71b1
vikash <vikashraj144@gmail.com>
2021-04-18 07:23:30 +0530

    get and add todo api added

--- backend/router/todoRoutes.js
+++ backend/router/todoRoutes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const Todo = require("./../models/ToDo");
 
 router.get("/", (req, res) => {
-  console.log("empty todo");
+  Todo.find((error, result) => {
+    if (error) throw new Error(error);
+    res.json(result);
+  });
+});
+
+router.post("/", (req, res) => {
+  Todo.create(req.body, (error, result) => {
+    if (error) throw new Error(error);
+    res.json(result);
+  });
 });
 
 module.exports = router;
